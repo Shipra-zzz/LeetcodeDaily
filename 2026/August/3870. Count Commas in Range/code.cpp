01f2f7f0de3ed6a2b6bc3ab25fcf0b2ca48932ae
Shipra-zzz <97395310+Shipra-zzz@users.noreply.github.com>
2026-09-08 17:48:28 +0530

    Create code.cpp for Count Commas in Range
    Added a new C++ file for the problem 'Count Commas in Range'.

---
+++ 2026/August/3870. Count Commas in Range/code.cpp
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-commas-in-range/description/?envType=daily-question&envId=2026-09-08
+
+class Solution {
+public:
+    int countCommas(int n) {
+        if(n<1000)  return 0;
+        return n-999;
+    }
+};
